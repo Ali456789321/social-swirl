cbab9b62f46211df872f40bd8f4af6dec45d1c6c
Ali456789321 <jan389719@gmail.com>
2024-03-01 19:17:27 +0500

    Updates

--- assets/Apply-DznDKzhd.js
+++
@@ -1,1 +0,0 @@
-import{j as p}from"./index-TQFE172R.js";const t=()=>p.jsx("div",{children:"Apply"});export{t as default};
---
+++ assets/Apply-_U9lGVe-.js
@@ -0,0 +1,1 @@
+import{j as p}from"./index-VdU94LB_.js";const t=()=>p.jsx("div",{children:"Apply"});export{t as default};
