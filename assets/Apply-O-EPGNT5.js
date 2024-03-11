31e8a4b8ee3ed037e16850936339b02e79ef7dde
Ali456789321 <jan389719@gmail.com>
2024-03-11 12:53:58 +0500

    Updates

--- assets/Apply-O-EPGNT5.js
+++
@@ -1,1 +0,0 @@
-import{j as p}from"./index-4HTLzY9b.js";const t=()=>p.jsx("div",{children:"Apply"});export{t as default};
---
+++ assets/Apply-SwY_fvqr.js
@@ -0,0 +1,1 @@
+import{j as p}from"./index-Y4SgCm5i.js";const t=()=>p.jsx("div",{children:"Apply"});export{t as default};
